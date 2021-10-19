2cff7a2a3632b3f7939bc30096d8f487864bf42b
just1aman <just1aman@gmail.com>
2021-10-19 23:24:29 +0000

    Initial commit

---
+++ DataStructures/index.js
@@ -0,0 +1,13 @@
+//lets create our own array
+
+class myArray{
+  constructor()
+  {
+  this.length=0
+  this.data={}
+
+  }
+}
+
+const AWSArray = New myArray();
+console.log(AWSArray)
---
+++ index.js
@@ -0,0 +1,12 @@
+function stylecode1(){
+  for(let i=1;i<10;i++){
+  console.log('Aman');
+  }
+}
+
+let primes = [2,3,5,7,11,13];
+
+stylecode2=array=>{for(const key in array){console.log(key);}}
+
+stylecode1();
+stylecode2(primes);
